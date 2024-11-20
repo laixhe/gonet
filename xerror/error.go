83be70f51ddcf8701d4610142f16d9bb38eef976
laixhe <laixhe@laixhe.com>
2024-11-20 11:17:14 +0800

    feat: 修改 error code 类型

--- xerror/error.go
+++ xerror/error.go
@@ -7,7 +7,7 @@ import (
 )
 
 type Error struct {
-	Code ecode.ECode
+	Code int32
 	Err  error
 }
 
@@ -18,7 +18,7 @@ func (e *Error) Error() string {
 	return e.Err.Error()
 }
 
-func NewError(code ecode.ECode, err error) *Error {
+func NewError(code int32, err error) *Error {
 	return &Error{
 		Code: code,
 		Err:  err,
@@ -27,84 +27,84 @@ func NewError(code ecode.ECode, err error) *Error {
 
 func ServiceError(err error) *Error {
 	return &Error{
-		Code: ecode.ECode_Service,
+		Code: int32(ecode.ECode_Service),
 		Err:  err,
 	}
 }
 
 func ServiceErrorStr(err string) *Error {
 	return &Error{
-		Code: ecode.ECode_Service,
+		Code: int32(ecode.ECode_Service),
 		Err:  errors.New(err),
 	}
 }
 
 func ParamError(err error) *Error {
 	return &Error{
-		Code: ecode.ECode_Param,
+		Code: int32(ecode.ECode_Param),
 		Err:  err,
 	}
 }
 
 func ParamErrorStr(err string) *Error {
 	return &Error{
-		Code: ecode.ECode_Param,
+		Code: int32(ecode.ECode_Param),
 		Err:  errors.New(err),
 	}
 }
 
 func TipError(err error) *Error {
 	return &Error{
-		Code: ecode.ECode_Tip,
+		Code: int32(ecode.ECode_Tip),
 		Err:  err,
 	}
 }
 
 func TipErrorStr(err string) *Error {
 	return &Error{
-		Code: ecode.ECode_Tip,
+		Code: int32(ecode.ECode_Tip),
 		Err:  errors.New(err),
 	}
 }
 
 func RepeatError(err error) *Error {
 	return &Error{
-		Code: ecode.ECode_Repeat,
+		Code: int32(ecode.ECode_Repeat),
 		Err:  err,
 	}
 }
 
 func RepeatErrorStr(err string) *Error {
 	return &Error{
-		Code: ecode.ECode_Repeat,
+		Code: int32(ecode.ECode_Repeat),
 		Err:  errors.New(err),
 	}
 }
 
 func AuthInvalidError(err error) *Error {
 	return &Error{
-		Code: ecode.ECode_AuthInvalid,
+		Code: int32(ecode.ECode_AuthInvalid),
 		Err:  err,
 	}
 }
 
 func AuthInvalidErrorStr(err string) *Error {
 	return &Error{
-		Code: ecode.ECode_AuthInvalid,
+		Code: int32(ecode.ECode_AuthInvalid),
 		Err:  errors.New(err),
 	}
 }
 
 func New(code int32, err error) *Error {
 	return &Error{
-		Code: ecode.ECode(code),
+		Code: code,
 		Err:  err,
 	}
 }
 
 func NewStr(code int32, err string) *Error {
 	return &Error{
-		Code: ecode.ECode(code),
+		Code: code,
 		Err:  errors.New(err),
 	}
 }
