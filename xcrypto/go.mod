3bd8159d566ee41b69114101da38dfe4c10c718d
laixhe <laixhe@laixhe.com>
2025-06-25 15:50:47 +0800

    feat: 添加加密功能模块

---
+++ xcrypto/go.mod
@@ -0,0 +1,5 @@
+module github.com/laixhe/gonet/xcrypto
+
+go 1.24
+
+require golang.org/x/crypto v0.39.0
---
+++ xcrypto/md5.go
@@ -0,0 +1,20 @@
+package xcrypto
+
+import (
+	"crypto/hmac"
+	"crypto/md5"
+	"encoding/hex"
+)
+
+// MD5 加密
+func MD5(data string) string {
+	md5Data := md5.Sum([]byte(data))
+	return hex.EncodeToString(md5Data[:])
+}
+
+// HmacMd5 带秘钥加密
+func HmacMd5(key string, data string) string {
+	mac := hmac.New(md5.New, []byte(key))
+	mac.Write([]byte(data))
+	return hex.EncodeToString(mac.Sum(nil))
+}
